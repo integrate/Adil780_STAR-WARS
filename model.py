379bd65ab7e7baedf28f317bb91b58b3c14d3e7b
Adil780 <fortito831@gmail.com>
2021-02-01 12:05:20 +0100

    VERSION 0.2: completed

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, settings
 pygame.init()
 
 ship = pygame.Rect(900, 650, 101, 110)
@@ -8,5 +8,9 @@ speedy = 10
 
 def move_ship_right():
     ship.x += 10
+    if ship.right >= settings.SCREEN_WIDTH:
+        ship.right = settings.SCREEN_WIDTH
 def move_ship_left():
     ship.x -= 10
+    if ship.left <= 0:
+        ship.left = 0
